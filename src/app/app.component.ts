657310edaf6a6c74f87786a649cc00f5250adbf2
Rohit Gaikwad <rohitgaikwad6830@gmail.com>
2023-02-09 20:49:42 +0530

    Code Added For Web OTP-AUTO-READ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,106 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'myApp';
+export class AppComponent implements OnInit, AfterViewInit {
+  title = 'ng-otp-auto-read';
+  constructor(private readonly updates: SwUpdate) {
+    if (this.updates.isEnabled) {
+      this.updates.available.subscribe((event) => {
+        alert('New update available, please reload window to continue');
+        window.location.reload();
+      });
+    }
+  }
+
+  ngOnInit() {}
+  errorMessage: any = '';
+  ngAfterViewInit() {
+    this.errorMessage = 'in ngAfterViewInit';
+    if ('OTPCredential' in window) {
+      this.mainObj.isWebOtpSupported = true;
+      this.errorMessage = 'in otp window';
+      window.addEventListener('DOMContentLoaded', (e) => {
+        this.errorMessage = 'in otp DOMContentLoaded';
+        const input = document.querySelector(
+          'input[autocomplete="one-time-code"]'
+        );
+        if (!input) return;
+        const ac = new AbortController();
+        var reqObj = {
+          otp: { transport: ['sms'] },
+          signal: ac.signal,
+        };
+        navigator.credentials
+          .get(reqObj)
+          .then((otp: any) => {
+            this.errorMessage = 'in otp credentials';
+            alert('GOT OTP***' + otp);
+            if (otp) {
+              if (otp && otp.code) {
+                alert('GOT OTP***' + otp.code);
+                this.myOTP = otp.code;
+              }
+            }
+          })
+          .catch((err) => {
+            this.errorMessage = 'in otp error';
+            console.log(err);
+          })
+      });
+    } else {
+      this.mainObj.isWebOtpSupported = false;
+      this.errorMessage = 'out of otp window';
+    }
+  }
+  myOTP: any;
+  mainObj: any = {};
+
+  code = `
+  errorMessage: any = '';
+  ngAfterViewInit() {
+    this.errorMessage = 'in ngAfterViewInit';
+    if ('OTPCredential' in window) {
+      this.mainObj.isWebOtpSupported = true;
+      this.errorMessage = 'in otp window';
+      window.addEventListener('DOMContentLoaded', (e) => {
+        this.errorMessage = 'in otp DOMContentLoaded';
+        const input = document.querySelector(
+          'input[autocomplete="one-time-code"]'
+        );
+        if (!input) return;
+        const ac = new AbortController();
+        var reqObj = {
+          otp: { transport: ['sms'] },
+          signal: ac.signal,
+        };
+        navigator.credentials
+          .get(reqObj)
+          .then((otp: any) => {
+            this.errorMessage = 'in otp credentials';
+            alert('GOT OTP***' + otp);
+            if (otp) {
+              if (otp && otp.code) {
+                alert('GOT OTP***' + otp.code);
+                this.myOTP = otp.code;
+              }
+            }
+          })
+          .catch((err) => {
+            this.errorMessage = 'in otp error';
+            console.log(err);
+          }).finally(() => {
+            this.errorMessage = 'in otp finally';
+          })
+      });
+    } else {
+      this.mainObj.isWebOtpSupported = false;
+      this.errorMessage = 'out of otp window';
+    }
+  }
+  `
 }
