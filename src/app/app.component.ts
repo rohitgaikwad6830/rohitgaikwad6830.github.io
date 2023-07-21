5e0e18049c894430e4e237a07cdc5ce56b475753
rohitgaikwad6830 <rohit.gaikwad@qorbital.com>
2023-07-21 18:27:51 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,106 +18,76 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getSms()
-    // this.errorMessage = 'in ngAfterViewInit';
-    // console.log('step 1');
-    // if ('OTPCredential' in window) {
+    this.errorMessage = 'in ngAfterViewInit';
+    console.log('step 1');
+    if ('OTPCredential' in window) {
 
-    //   this.mainObj.isWebOtpSupported = true;
-    //   this.errorMessage = 'in otp window';
-    //   window.addEventListener('DOMContentLoaded', (e) => {
-    //     this.errorMessage = 'in otp DOMContentLoaded';
-    //     console.log('step 2 inside the dom otp');
-    //     debugger
-    //     const input = document.querySelector(
-    //       'input[autocomplete="one-time-code"]'
-    //     );
-    //     alert("above the return");
-    //     // if (!input) return;
-    //     const ac = new AbortController();
-    //     debugger
-    //     console.log('step 3 inside the controler');
-    //     var reqObj = {
-    //       otp: { transport: ['sms']}, // Replace the regex with your desired format
-    //       signal: ac.signal,
-    //     };
-    //     console.log('req obj',reqObj);
-    //     debugger
-    //     navigator.credentials
-    //       .get(reqObj)
-    //       .then((otp: any) => {
-    //         console.log('step 4 inside get message');
-    //         if (otp) {
-    //           console.log('step 5 inside the got otp');
-    //           alert('in OTP window***' + otp);
-    //           this.errorMessage = 'in otp credentials';
-    //           const otpRegex = /OTP for Aadhaar is ([0-9]{6})/; // Replace the regex with your desired format
-    //           const match = otpRegex.exec(otp.toString());
-    //           alert('GOT OTP***' + otp);
-    //           alert('Got match'+ match)
-    //           if (match && match[1]) {
-    //             const extractedOTP = match[1];
-    //             // Do something with the extractedOTP, e.g., assign it to an input field
-    //             (document.getElementById("otpInputId") as HTMLInputElement).value = extractedOTP;
-    //             this.myOTP = extractedOTP;
-    //             // Trigger the OTP submission, if needed
-    //             (document?.getElementById("otpButtonId") as any)?.click();
-    //             alert('GOT OTP***' + otp.code);
-    //           } else {
-    //             // Handle the case when OTP format doesn't match
-    //             this.errorMessage = 'OTP format not recognized.';
-    //           }
-    //         }
-    //       })
-    //       // .then((otp: any) => {
-    //       //   this.errorMessage = 'in otp credentials';
-    //       //   alert('GOT OTP***' + otp);
-    //       //   if (otp) {
-    //       //     if (otp && otp.code) {
-    //       //       alert('GOT OTP***' + otp.code);
-    //       //       this.myOTP = otp.code;
-    //       //     }
-    //       //   }
-    //       // })
-    //       .catch((err) => {
-    //         this.errorMessage = 'in otp error';
-    //         console.log(err);
-    //       })
-    //   });
-    // } else {
-    //   this.mainObj.isWebOtpSupported = false;
-    //   this.errorMessage = 'out of otp window';
-    // }
-  }
-
-  getSms() {
-    if ('serviceWorker' in navigator && 'MessageChannel' in window) {
-      navigator.serviceWorker
-        .getRegistration()
-        .then(registration => {
-          if (registration) {
-            const channel = new MessageChannel();
-            channel.port1.onmessage = event => {
-              const otp = event.data; // Assuming the OTP is received as a plain text message
-              const otpElement = document.getElementById('otp') as HTMLInputElement;
-              if (otpElement) {
-                otpElement.value = otp;
+      this.mainObj.isWebOtpSupported = true;
+      this.errorMessage = 'in otp window';
+      window.addEventListener('DOMContentLoaded', (e) => {
+        this.errorMessage = 'in otp DOMContentLoaded';
+        console.log('step 2 inside the dom otp');
+        debugger
+        const input = document.querySelector(
+          'input[autocomplete="one-time-code"]'
+        );
+        alert("above the return");
+        // if (!input) return;
+        const ac = new AbortController();
+        debugger
+        console.log('step 3 inside the controler');
+        var reqObj = {
+          otp: { transport: ['sms']}, // Replace the regex with your desired format
+          signal: ac.signal,
+        };
+        console.log('req obj',reqObj);
+        debugger
+        navigator.credentials
+          .get(reqObj)
+          .then((otp: any) => {
+            console.log('step 4 inside get message');
+            if (otp) {
+              console.log('step 5 inside the got otp');
+              alert('in OTP window***' + otp);
+              this.errorMessage = 'in otp credentials';
+              const otpRegex = /OTP for Aadhaar is ([0-9]{6})/; // Replace the regex with your desired format
+              const match = otpRegex.exec(otp.toString());
+              alert('GOT OTP***' + otp);
+              alert('Got match'+ match)
+              if (match && match[1]) {
+                const extractedOTP = match[1];
+                // Do something with the extractedOTP, e.g., assign it to an input field
+                (document.getElementById("otpInputId") as HTMLInputElement).value = extractedOTP;
+                this.myOTP = extractedOTP;
+                // Trigger the OTP submission, if needed
+                (document?.getElementById("otpButtonId") as any)?.click();
+                alert('GOT OTP***' + otp.code);
+              } else {
+                // Handle the case when OTP format doesn't match
+                this.errorMessage = 'OTP format not recognized.';
               }
-            };
-            registration.active?.postMessage('getSms', [channel.port2]);
-          } else {
-            console.warn('Service worker registration not found.');
-          }
-        })
-        .catch(error => {
-          console.error('Error accessing service worker:', error);
-        });
+            }
+          })
+          // .then((otp: any) => {
+          //   this.errorMessage = 'in otp credentials';
+          //   alert('GOT OTP***' + otp);
+          //   if (otp) {
+          //     if (otp && otp.code) {
+          //       alert('GOT OTP***' + otp.code);
+          //       this.myOTP = otp.code;
+          //     }
+          //   }
+          // })
+          .catch((err) => {
+            this.errorMessage = 'in otp error';
+            console.log(err);
+          })
+      });
     } else {
-      console.warn('Service workers or MessageChannel are not supported in this browser.');
+      this.mainObj.isWebOtpSupported = false;
+      this.errorMessage = 'out of otp window';
     }
   }
-
-
   errorMessage: any = '';
 
   myOTP: any;
